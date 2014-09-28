85265ca586e60cc933e8ee57abd9b96b424b3ff5
Justas <rujustas@gmail.com>
2014-09-28 13:52:53 +0300

    created topic spec

---
+++ spec/topic_spec.rb
@@ -0,0 +1,15 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'spec_helper'
+
+describe Topic do
+  it 'should have title' do
+  end
+
+  it 'should have owner' do
+  end
+
+  it "should have first message written by owner" do
+  end  
+end
