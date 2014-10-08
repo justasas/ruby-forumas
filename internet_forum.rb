bfa764c1c64b494db11ed643059a07e5dd79eef9
Justas <rujustas@gmail.com>
2014-10-08 14:31:11 +0300

    prijungima padariau

--- internet_forum.rb
+++ internet_forum.rb
@@ -9,7 +9,17 @@ class Internet_forum
   end
 
   def register_new_user(user)
-    return false if !accounts.index(user).nil?
+    accounts.each { |account| return false if account.name == user.name }
     accounts.push user
+  end
+
+  def log_in(name, pass)
+    found_account = nil
+    accounts.each do |account|
+      found_account = account if account.name == name
+      return 'user does not exist' if found_account == nil
+      return 'password is not correct' if found_account.pass != pass
+      return found_account.name
+    end
   end
 end
